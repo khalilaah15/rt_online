9753ba43a1df0fc41b80929cfcc8b4ad72f2866c
Shabil Alqorni <shabilalqorni@Shabils-MacBook-Pro.local>
2023-10-12 10:09:48 +0700

    Add Flutter Util Screen

--- lib/configs/pages/page.dart
+++ lib/configs/pages/page.dart
@@ -3,7 +3,7 @@ import 'package:venturo_core/configs/routes/route.dart';
 import 'package:venturo_core/features/splash/bindings/splash_binding.dart';
 import 'package:venturo_core/features/splash/view/ui/splash_screen.dart';
 
-abstract class AppPages {
+abstract class Pages {
   static final pages = [
     GetPage(
       name: Routes.splashRoute,
---
+++ lib/shared/styles/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  ColorStyle._();
+
+  static const Color primary = Color(0xff2C8BA4);
+}
